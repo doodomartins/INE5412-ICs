10a6d8ed468874d986549b5c47bb90777495a0b1
Douglas Martins <martins.doug30@gmail.com>
2016-03-17 22:54:49 -0300

    Iniciado IC1.

---
+++ IC1/main.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h> 
+#include <sys/wait.h>
+// #include <sys/>
+
+using namespace std;
+int main ()
+{
+   // include declarations you need
+   int count = 0;
+   int i;
+   pid_t pid;
+   /* Repeat 5 times */
+   for(i = 0; i < 5; i++){
+      cout << "Parent process " << getpid() << ": Creating child" << endl;
+      /* Duplicate this process */
+      pid = fork();
+      /* If there was an error on duplication then */
+      if(pid < 0){
+         cout << "Error on fork()" << endl;
+         return -1; 
+      } 
+      /* If child-process is running then */
+      if(pid == 0){
+         cout << "Child process " << getpid() << ": Running" << endl;
+         count++;
+         /* Sleep for 1 second */
+         sleep(2);
+         cout << "Child process " << getpid() << ": Exiting with status " << count << endl;
+         /*Faz com que todos processos que são filhos acabem aqui.*/
+         exit(count);
+      }
+   }
+
+   /* if this is the parent-process then */
+   cout << "Parent process " << getpid() << ": Waiting children to exit" << endl;
+   int errno, status, sum = 0;
+   /* Parent-process waits for all children to exit, adding each status to the sum variable */
+   /* O primeiro parametro é 0 pois estou fazendo o waitpid() em todos os processos filhos.*/
+   while(waitpid(0, &status, 0) != -1){
+      sum += WEXITSTATUS(status);
+   }
+
+   cout << "Parent process " << getpid() << ": Exiting with sum " <<sum << endl;
+   /*  */
+   return count;
+}
